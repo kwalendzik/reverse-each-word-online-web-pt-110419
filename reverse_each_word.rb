cb6437a8c84a2edb6edf63ef64c0c54774139412
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-14 19:19:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence)
   end
   sentence_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(/ /)
+  sentence_array.each do |sentence_array| 
+    sentence_array.reverse!
+  end
+  sentence_array.join(" ")
+end
